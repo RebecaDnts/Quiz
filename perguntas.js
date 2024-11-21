772848d4353d3d3c0b9445e3f2f8e1274dcf0556
Rebeca Dantas <00001063661493sp@al.educacao.sp.gov.br>
2024-11-21 11:46:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Aniversário',
+    'Quando o Harry nasceu?',
+    '1 de fevereiro de 1994'
+)
+
+criaCartao(
+    'Origem',
+    'Onde o Harry nasceu?',
+    'Redditch, Inglaterra, Reino Unido'
+)
+
+criaCartao(
+    'Nome',
+    'Qual o nome completo do Harry Styles?',
+    'Harry Edward Styles'
+)
+
+criaCartao(
+    'Músicas',
+    'Qual o melhor álbum que ele já lançou?',
+    'Harrys House'
+)
+
+criaCartao(
+    'Amor',
+    'Quais as suas ex mais famosas?',
+    'Kendall Jenner e Taylor Swift'
+)
+
+criaCartao(
+    'Tattoo',
+    'Qual a tatuagem mais bonita do Harry?',
+    'Uma borboleta'
+)
+
+criaCartao(
+    'Brothers',
+    'Qual o integrante favorito do Harry no One Direction?',
+    'Ele mesmo'
+)
+
+criaCartao(
+    'Videoclipes',
+    'Quais os videoclipes mais bem produzidos do Harry Styles?',
+    'Clipe de Adore U e Falling'
+)
+
+criaCartao(
+    'Letras',
+    'Quais as músicas com as melhores letras?',
+    'Late Night Talking e Matilda'
+)
